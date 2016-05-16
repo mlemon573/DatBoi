e8a70c44d0d89ab964ec00bfa27c5ed65b485a20
mlemon573 <mlemoine573@gmail.com>
2016-05-16 01:16:05 -0700

    fix modelListener

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,79 +1,90 @@
-import java.util.*;
-import java.io.*;
-import java.util.List;
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Canvas extends JPanel implements ModelListener {
-	private List<DShape> shapes;
-	private DShape selected;
-	private int selectedX;
-	private int selectedY;
-	private int id;
+public class Canvas extends JPanel implements ModelListener
+{
+   private List<DShape> shapes;
+   private DShape selected;
+   private int selectedX;
+   private int selectedY;
+   private int id;
 
-	public Canvas() {
-		this(400, 400);
-	}
+   public Canvas()
+   {
+      this(400, 400);
+   }
 
-	public Canvas(int width, int height) {
-		this.setPreferredSize(new Dimension(width, height));
-		this.setOpaque(true);
-		this.setBackground(Color.white);
-		shapes = new ArrayList<DShape>();
-		id = 1;
-	}
+   public Canvas(int width, int height)
+   {
+      this.setPreferredSize(new Dimension(width, height));
+      this.setOpaque(true);
+      this.setBackground(Color.white);
+      shapes = new ArrayList<>();
+      id = 1;
+   }
 
-	public void addShape(DShape shape) {
-		shape.setID(id++);
-		shapes.add(shape);
-		selected = shape;
-		repaint();
-	}
+   public void addShape(DShape shape)
+   {
+      shapes.add(shape);
+      shape.addListener(this);
+      shape.setID(id++);
+      selected = shape;
+      repaint();
+   }
 
-	public void removeSelected() {
-		if (shapes.contains(selected)) {
-			shapes.remove(selected);
-			id--;
-			selected = null;
-			repaint();
-		}
-	}
+   public void removeSelected()
+   {
+      if (shapes.contains(selected))
+      {
+         shapes.remove(selected);
+         selected = null;
+         repaint();
+      }
+   }
 
-	public void clear() {
-		shapes.clear();
-		selected = null;
-		repaint();
-	}
+   public void clear()
+   {
+      shapes.clear();
+      selected = null;
+      repaint();
+   }
 
-	public void setSelected(DShape shape) {
-		selected = shape;
-	}
+   public DShape getSelected()
+   {
+      return this.selected;
+   }
 
-	public DShape getSelected() {
-		return this.selected;
-	}
-	
-	public List<DShape> getShapesList() {
-		return shapes;
-	}
+   public void setSelected(DShape shape)
+   {
+      selected = shape;
+   }
 
-	public DShape findShape(int x, int y) {
-		return null;
-	}
+   public List<DShape> getShapesList()
+   {
+      return shapes;
+   }
 
-	public void moveShape(DShape shape, int newX, int newY) {
-		
-	}
+   public DShape findShape(int x, int y)
+   {
+      return null;
+   }
 
-	@Override
-	public void modelChanged(DShapeModel model) {
-		repaint();
-	}
+   public void moveShape(DShape shape, int newX, int newY)
+   {
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		for (DShape shape : shapes) {
-			shape.draw(g);
-		}
-	}
+   }
+
+   @Override
+   public void modelChanged(DShapeModel model)
+   {
+      repaint();
+   }
+
+   public void paintComponent(Graphics g)
+   {
+      super.paintComponent(g);
+      for (DShape shape : shapes) {shape.draw(g);}
+   }
 }
