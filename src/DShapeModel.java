147cacbead9e362c00f6bdea515880d0ce601163
Michael Huang <Quagsire@users.noreply.github.com>
2016-05-16 22:13:17 -0700

    Text changes added.

--- src/DShapeModel.java
+++ src/DShapeModel.java
@@ -2,179 +2,168 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DShapeModel
-{
-   private int x;
-   private int y;
-   private int width;
-   private int height;
-   private Color color;
-   private int id;
-   private List<ModelListener> listeners;
+public class DShapeModel {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private Color color;
+	private int id;
+	private List<ModelListener> listeners;
 
-   public DShapeModel()
-   {
-      this(0, 0, 0, 0, Color.GRAY, 0);
-      listeners = new ArrayList<>();
-   }
+	public DShapeModel() {
+		this(0, 0, 0, 0, Color.GRAY, 0);
+		listeners = new ArrayList<>();
+	}
 
-   public DShapeModel(int x, int y, int width, int height, int id)
-   {
-      this(x, y, width, height, Color.GRAY, 0);
-   }
+	public DShapeModel(int x, int y, int width, int height, int id) {
+		this(x, y, width, height, Color.GRAY, 0);
+	}
 
-   public DShapeModel(int x, int y, int width, int height, Color color, int id)
-   {
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      this.color = color;
-   }
+	public DShapeModel(int x, int y, int width, int height, Color color, int id) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
 
-   public void addListener(ModelListener listener)
-   {
-      listeners.add(listener);
-   }
+	public void addListener(ModelListener listener) {
+		listeners.add(listener);
+	}
 
-   public void notifyListeners()
-   {
-      for (ModelListener ml : listeners) {ml.modelChanged(this);}
-   }
+	public void notifyListeners() {
+		for (ModelListener ml : listeners) {
+			ml.modelChanged(this);
+		}
+	}
 
-   public Color getColor()
-   {
-      return color;
-   }
+	public Color getColor() {
+		return color;
+	}
 
-   public void setColor(Color color)
-   {
-      this.color = color;
-      notifyListeners();
-   }
+	public void setColor(Color color) {
+		this.color = color;
+		notifyListeners();
+	}
 
-   public int getX()
-   {
-      return x;
-   }
+	public int getX() {
+		return x;
+	}
 
-   public void setX(int x)
-   {
-      this.x = x;
-      notifyListeners();
-   }
+	public void setX(int x) {
+		this.x = x;
+		notifyListeners();
+	}
 
-   public int getY()
-   {
-      return y;
-   }
+	public int getY() {
+		return y;
+	}
 
-   public void setY(int y)
-   {
-      this.y = y;
-      notifyListeners();
-   }
+	public void setY(int y) {
+		this.y = y;
+		notifyListeners();
+	}
 
-   public int getWidth()
-   {
-      return this.width;
-   }
+	public int getWidth() {
+		return this.width;
+	}
 
-   public void setWidth(int width)
-   {
-      this.width = width;
-      notifyListeners();
-   }
+	public void setWidth(int width) {
+		this.width = width;
+		notifyListeners();
+	}
 
-   public int getHeight()
-   {
-      return this.height;
-   }
+	public int getHeight() {
+		return this.height;
+	}
 
-   public void setHeight(int height)
-   {
-      this.height = height;
-      notifyListeners();
-   }
+	public void setHeight(int height) {
+		this.height = height;
+		notifyListeners();
+	}
 
-   public int getID()
-   {
-      return id;
-   }
+	public int getID() {
+		return id;
+	}
 
-   public void setID(int id)
-   {
-      this.id = id;
-   }
+	public void setID(int id) {
+		this.id = id;
+	}
 
-   public void moveBy(int dx, int dy)
-   {
-      x += dx;
-      y += dy;
-      notifyListeners();
-   }
+	public void moveBy(int dx, int dy) {
+		x += dx;
+		y += dy;
+		notifyListeners();
+	}
 
-   public void setXY(int x, int y)
-   {
-      this.x = x;
-      this.y = y;
-      notifyListeners();
-   }
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+		notifyListeners();
+	}
 
-   public void setBounds(int x, int y, int width, int height)
-   {
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      notifyListeners();
-   }
+	public void setBounds(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		notifyListeners();
+	}
 
-   public Rectangle getBounds()
-   {
-      return new Rectangle(x, y, width, height);
-   }
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, width, height);
+	}
 
-   public boolean equals(DShapeModel model)
-   {
-      if (id == model.getID() && getClass().equals(model.getClass())
-            && this.getBounds().equals(model.getBounds())
-            && color.equals(model.getColor()))
-      {
-         return true;
-      }
-      return false;
-   }
+	public boolean equals(DShapeModel model) {
+		if (id == model.getID()) {
+			return true;
+		}
+		return false;
+	}
 }
 
-class DRectModel extends DShapeModel
-{
-   public DRectModel()
-   {
-      super();
-   }
+class DRectModel extends DShapeModel {
+	public DRectModel() {
+		super();
+	}
 }
 
-class DOvalModel extends DShapeModel
-{
-   public DOvalModel()
-   {
-      super();
-   }
+class DOvalModel extends DShapeModel {
+	public DOvalModel() {
+		super();
+	}
 }
 
-class DLineModel extends DShapeModel
-{
-   public DLineModel()
-   {
-      super();
-   }
+class DLineModel extends DShapeModel {
+	public DLineModel() {
+		super();
+	}
 }
 
-class DTextModel extends DShapeModel
-{
-   public DTextModel()
-   {
-      super();
-   }
+class DTextModel extends DShapeModel {
+	private String text;
+	private Font font;
+	
+	public DTextModel() {
+		super();
+		this.text = "";
+		this.font = null;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public String getText() {
+		return this.text;
+	}
+	
+	public void setFont(Font font) {
+		this.font = font;
+	}
+	
+	public Font getFont() {
+		return this.font;
+	}
 }
