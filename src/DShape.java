33e5a34dbb7a503754ed163e7e083e39656934e1
Michael Huang <Quagsire@users.noreply.github.com>
2016-05-16 22:12:46 -0700

    Oval and Text drawing added.

--- src/DShape.java
+++ src/DShape.java
@@ -2,169 +2,155 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class DShape
-{
-   private DShapeModel model;
+public abstract class DShape {
+	private DShapeModel model;
 
-   public DShape()
-   {
+	public DShape() {
 
-   }
+	}
 
-   public int getX()
-   {
-      return model.getX();
-   }
+	public int getX() {
+		return model.getX();
+	}
 
-   public void setX(int x)
-   {
-      model.setX(x);
-   }
+	public void setX(int x) {
+		model.setX(x);
+	}
 
-   public int getY()
-   {
-      return model.getY();
-   }
+	public int getY() {
+		return model.getY();
+	}
 
-   public void setY(int y)
-   {
-      model.setY(y);
-   }
+	public void setY(int y) {
+		model.setY(y);
+	}
 
-   public int getWidth()
-   {
-      return model.getWidth();
-   }
+	public int getWidth() {
+		return model.getWidth();
+	}
 
-   public void setWidth(int width)
-   {
-      model.setWidth(width);
-   }
+	public void setWidth(int width) {
+		model.setWidth(width);
+	}
 
-   public int getHeight()
-   {
-      return model.getHeight();
-   }
+	public int getHeight() {
+		return model.getHeight();
+	}
 
-   public void setHeight(int height)
-   {
-      model.setHeight(height);
-   }
+	public void setHeight(int height) {
+		model.setHeight(height);
+	}
 
-   public Color getColor()
-   {
-      return model.getColor();
-   }
+	public Color getColor() {
+		return model.getColor();
+	}
 
-   public void setColor(Color color)
-   {
-      model.setColor(color);
-   }
+	public void setColor(Color color) {
+		model.setColor(color);
+	}
 
-   public int getID()
-   {
-      return model.getID();
-   }
+	public int getID() {
+		return model.getID();
+	}
 
-   public void setID(int id)
-   {
-      model.setID(id);
-   }
+	public void setID(int id) {
+		model.setID(id);
+	}
 
-   public void setBounds(int x, int y, int width, int height)
-   {
-      model.setBounds(x, y, width, height);
-   }
+	public void setBounds(int x, int y, int width, int height) {
+		model.setBounds(x, y, width, height);
+	}
 
-   public Rectangle getBounds()
-   {
-      return model.getBounds();
-   }
+	public Rectangle getBounds() {
+		return model.getBounds();
+	}
 
-   public boolean equals(DShape shape)
-   {
-      return shape != null && this.model.equals(shape.getModel());
-   }
+	public boolean equals(DShape shape) {
+		return shape != null && this.model.equals(shape.getModel());
+	}
 
-   public DShapeModel getModel()
-   {
-      return this.model;
-   }
+	public DShapeModel getModel() {
+		return this.model;
+	}
 
-   public void setModel(DShapeModel model)
-   {
-      this.model = model;
-   }
+	public void setModel(DShapeModel model) {
+		this.model = model;
+	}
 
-   public void addListener(ModelListener listener)
-   {
-      model.addListener(listener);
-   }
+	public void addListener(ModelListener listener) {
+		model.addListener(listener);
+	}
 
-   abstract void draw(Graphics g);
+	abstract void draw(Graphics g);
 
-   protected List<Point> getKnobs()
-   {
-      Rectangle r = model.getBounds();
-      List<Point> knobs = new ArrayList<Point>();
-      knobs.add(new Point((int) (r.getX()), (int) (r.getY())));
-      knobs.add(new Point((int) (r.getX() + r.getWidth()), (int) r.getY()));
-      knobs.add(new Point((int) (r.getX()), (int) (r.getY() + r.getHeight())));
-      knobs.add(new Point((int) (r.getX() + r.getWidth()), (int) (r.getY()
-            + r.getHeight())));
-      return knobs;
-   }
+	protected List<Point> getKnobs() {
+		Rectangle r = model.getBounds();
+		List<Point> knobs = new ArrayList<Point>();
+		knobs.add(new Point((int) (r.getX()), (int) (r.getY())));
+		knobs.add(new Point((int) (r.getX() + r.getWidth()), (int) r.getY()));
+		knobs.add(new Point((int) (r.getX()), (int) (r.getY() + r.getHeight())));
+		knobs.add(new Point((int) (r.getX() + r.getWidth()), (int) (r.getY() + r.getHeight())));
+		return knobs;
+	}
 }
 
-class DRect extends DShape
-{
-   public DRect()
-   {
-      super.setModel(new DRectModel());
-   }
+class DRect extends DShape {
+	public DRect() {
+		super.setModel(new DRectModel());
+	}
 
-   public void draw(Graphics g)
-   {
-      g.setColor(this.getColor());
-      g.fillRect(getX(), getY(), getWidth(), getHeight());
-   }
+	public void draw(Graphics g) {
+		g.setColor(this.getColor());
+		g.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
 }
 
-class DOval extends DShape
-{
-   public DOval()
-   {
-      super.setModel(new DOvalModel());
-   }
+class DOval extends DShape {
+	public DOval() {
+		super.setModel(new DOvalModel());
+	}
 
-   public void draw(Graphics g)
-   {
-
-   }
+	public void draw(Graphics g) {
+		g.setColor(this.getColor());
+		g.fillOval(getX(), getY(), getWidth(), getHeight());
+	}
 }
 
-class DLine extends DShape
-{
-   public DLine()
-   {
-      super.setModel(new DLineModel());
-   }
+class DLine extends DShape {
+	public DLine() {
+		super.setModel(new DLineModel());
+	}
 
-   public void draw(Graphics g)
-   {
-
-   }
+	public void draw(Graphics g) {
+		
+	}
 }
 
-class DText extends DShape
-{
-   public DText()
-   {
-      super.setModel(new DLineModel());
-   }
+class DText extends DShape {
 
-   public void draw(Graphics g)
-   {
+	public DText() {
+		super.setModel(new DTextModel());
+	}
 
-   }
+	public void setText(String text) {
+		DTextModel model = (DTextModel) getModel();
+		model.setText(text);
+	}
+
+	public String getText() {
+		return ((DTextModel) getModel()).getText();
+	}
+
+	public void setFont(Font font) {
+		((DTextModel) getModel()).setFont(font);
+	}
+
+	public Font getFont() {
+		return ((DTextModel) getModel()).getFont();
+	}
+
+	public void draw(Graphics g) {
+		g.setColor(this.getColor());
+		g.drawString(getText(), getX() + 4, getY() + getHeight() - 4);
+	}
 }
