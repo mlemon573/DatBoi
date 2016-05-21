c0b005a2cc60a5542a3474763eb27b308498dca1
A SMALL LOAN OF A MILLION DOLLARS <Quagsire@users.noreply.github.com>
2016-05-21 15:31:09 -0700

    Font variable in model is now a string.

--- src/DTextModel.java
+++ src/DTextModel.java
@@ -1,36 +1,34 @@
-import java.awt.*;
+import java.awt.Font;
 
-public class DTextModel extends DShapeModel
-{
-   private String text;
-   private Font font;
-
-   public DTextModel()
-   {
-      super();
-      this.text = "";
-      this.font = null;
-   }
-
-   public String getText()
-   {
-      return this.text;
-   }
-
-   public void setText(String text)
-   {
-      this.text = text;
-      notifyListeners();
-   }
-
-   public Font getFont()
-   {
-      return this.font;
-   }
-
-   public void setFont(String font)
-   {
-      this.font = new Font(font, Font.PLAIN, 14);
-      notifyListeners();
-   }
+public class DTextModel extends DShapeModel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -586348795511193029L;
+	private String text;
+	private String font;
+	
+	public DTextModel() {
+		super();
+		this.text = "";
+		this.font = null;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+		notifyListeners();
+	}
+	
+	public String getText() {
+		return this.text;
+	}
+	
+	public void setFont(String font) {
+		this.font = font;
+		notifyListeners();
+	}
+	
+	public String getFont() {
+		return this.font;
+	}
 }
