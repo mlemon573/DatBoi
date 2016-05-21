cfd2bf0e7f2d8203408f04cd362f07f204562b4e
A SMALL LOAN OF A MILLION DOLLARS <Quagsire@users.noreply.github.com>
2016-05-21 15:30:28 -0700

    Text updated.

--- src/DText.java
+++ src/DText.java
@@ -1,38 +1,53 @@
-import java.awt.*;
+import java.awt.Font;
+import java.awt.Shape;
+import java.awt.Graphics;
 
-public class DText extends DShape
-{
+public class DText extends DShape {
 
-   public DText()
-   {
-      super.setModel(new DTextModel());
-   }
+	public DText() {
+		super.setModel(new DTextModel());
+	}
 
-   public String getText()
-   {
-      return ((DTextModel) getModel()).getText();
-   }
+	public void setText(String text) {
+		DTextModel model = (DTextModel) getModel();
+		model.setText(text);
+	}
 
-   public void setText(String text)
-   {
-      DTextModel model = (DTextModel) getModel();
-      model.setText(text);
-   }
+	public String getText() {
+		return ((DTextModel) getModel()).getText();
+	}
 
-   public Font getFont()
-   {
-      return ((DTextModel) getModel()).getFont();
-   }
+	public void setFont(String font) {
+		((DTextModel) getModel()).setFont(font);
+	}
 
-   public void setFont(String font)
-   {
-      ((DTextModel) getModel()).setFont(font);
-   }
+	public String getFont() {
+		return ((DTextModel) getModel()).getFont();
+	}
+	
+	public Font computeFont(Graphics g) {
+		Font font = g.getFont();
+		int maxHeight = getModel().getHeight();
+		int prevSize = font.getSize();
+		for (double size = 1.0; ; size = (size * 1.10) + 1) {
+			font = new Font(getFont(), Font.PLAIN, (int) size);
+			int height = g.getFontMetrics(font).getHeight();
+			if (height < maxHeight) {
+				prevSize = (int) size;
+			} else {
+				font = new Font(font.getName(), Font.PLAIN, prevSize);
+				return font;
+			}
+		}
+	}
 
-   public void draw(Graphics g)
-   {
-      g.setColor(this.getColor());
-      g.setFont(getFont());
-      g.drawString(getText(), getX(), getY() + getHeight() - 6);
-   }
+	public void draw(Graphics g) {
+		g.setColor(this.getColor());
+		g.setFont(new Font(getFont(), Font.PLAIN, 1));
+		g.setFont(computeFont(g));
+		Shape clip = g.getClip();
+		g.setClip(clip.getBounds().createIntersection(getBounds()));
+		g.drawString(getText(), getX(), getY() + getHeight() - g.getFontMetrics().getDescent());
+		g.setClip(clip);
+	}
 }
