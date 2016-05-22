a4c0dd6fce4ba6d288222cc1c0bd2f2f73f47ec2
GevorkianRay <gevorkianray@gmail.com>
2016-05-22 08:00:56 -0700

    javadoc

--- src/DRect.java
+++ src/DRect.java
@@ -1,12 +1,22 @@
 import java.awt.*;
 
+/**
+ * Class to handle the drawing of the oval shape.
+ */
 public class DRect extends DShape
 {
+   /**
+    * Constructor for DRect.
+    */
    public DRect()
    {
       super.setModel(new DRectModel());
    }
 
+   /**
+    * Method to draw the rectangle with a respective position, size, and color.
+    * @param g - the graphic to draw.
+    */
    public void draw(Graphics g)
    {
       g.setColor(this.getColor());
