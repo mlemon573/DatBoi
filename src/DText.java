fc54f50edf4d350cef774d018207d60ec4ecf1ab
GevorkianRay <gevorkianray@gmail.com>
2016-05-22 08:11:18 -0700

    stuff

--- src/DText.java
+++ src/DText.java
@@ -1,34 +1,62 @@
 import java.awt.*;
 
+/**
+ * Class to handle the drawing and manipulation of text.
+ */
 public class DText extends DShape
 {
 
+   /**
+    * Constructor for DText.
+    */
    public DText()
    {
       super.setModel(new DTextModel());
    }
 
+   /**
+    * Getter method for DText.
+    * @return the text as a DTextModel.
+     */
    public String getText()
    {
       return ((DTextModel) getModel()).getText();
    }
 
+   /**
+    * Setter method for DText
+    * @param text - the text to set for the model.
+     */
    public void setText(String text)
    {
       DTextModel model = (DTextModel) getModel();
       model.setText(text);
    }
 
+   /**
+    * Getter method for DText.
+    * @return the font as a DTextModel.
+    */
+
    public String getFont()
    {
       return ((DTextModel) getModel()).getFont();
    }
 
+   /**
+    * Setter method for DText.
+    * @param font - the font to set for the model.
+     */
    public void setFont(String font)
    {
       ((DTextModel) getModel()).setFont(font);
    }
 
+   /**
+    * Method to compute the size that the font should have.
+    * @param g - the graphic to draw.
+    * @return - the font to set for the model.
+     */
    public Font computeFont(Graphics g)
    {
       Font font = g.getFont();
@@ -50,6 +78,10 @@ public class DText extends DShape
       }
    }
 
+   /**
+    * Method to draw DText on the canvas.
+    * @param g - the graphic to draw.
+     */
    public void draw(Graphics g)
    {
       g.setColor(this.getColor());
