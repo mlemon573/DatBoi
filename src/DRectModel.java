25d54bc4bbdf7b0512a7edcd9ba7741c86e1e260
GevorkianRay <gevorkianray@gmail.com>
2016-05-22 07:44:48 -0700

    here comes dat boi

--- src/DRectModel.java
+++ src/DRectModel.java
@@ -1,5 +1,12 @@
+/**
+ * Model class for the DRect class.
+ * To keep track of all the numerical information and properties of the respective shape.
+ */
 public class DRectModel extends DShapeModel
 {
+   /**
+    * Constructor for DRectModel.
+    */
    public DRectModel()
    {
       super();
