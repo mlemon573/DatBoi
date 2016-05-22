a2fcdfad370f493a3354365050b9be95ec97b8d4
GevorkianRay <gevorkianray@gmail.com>
2016-05-22 07:59:31 -0700

    changes n stoof

--- src/DOval.java
+++ src/DOval.java
@@ -1,12 +1,21 @@
 import java.awt.*;
-
+/**
+ * Class to handle the drawing of the oval shape.
+ */
 public class DOval extends DShape
 {
+   /**
+    * Constructor for DOval.
+    */
    public DOval()
    {
       super.setModel(new DOvalModel());
    }
 
+   /**
+    * Method to draw the oval with a respective position, size, and color.
+    * @param g - the graphic to draw.
+     */
    public void draw(Graphics g)
    {
       g.setColor(this.getColor());
