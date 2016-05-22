3e835d10606604a693754e142940a86d9360f77f
GevorkianRay <gevorkianray@gmail.com>
2016-05-22 07:46:23 -0700

    oh shit waddup!

--- src/DOvalModel.java
+++ src/DOvalModel.java
@@ -1,5 +1,12 @@
+/**
+ * Model class for the DOval class.
+ * To keep track of all the numerical information and properties of the respective shape.
+ */
 public class DOvalModel extends DShapeModel
 {
+   /**
+    * Constructor for DOvalModel.
+    */
    public DOvalModel()
    {
       super();
