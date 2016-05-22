8df4d7d00ed7464ac781d8a3fd21a16a04f19825
GevorkianRay <gevorkianray@gmail.com>
2016-05-22 07:57:32 -0700

    updates and stuff

--- src/DTextModel.java
+++ src/DTextModel.java
@@ -1,12 +1,17 @@
+/**
+ * Model class for the DText class.
+ * To keep track of all the numerical information and properties of the respective shape.
+ * To set the Text and Font of DText appropriately.
+ */
 public class DTextModel extends DShapeModel
 {
-   /**
-    *
-    */
-   private static final long serialVersionUID = -586348795511193029L;
+   //DText elements
    private String text;
    private String font;
 
+   /**
+    * Constructor for the DTextModel.
+    */
    public DTextModel()
    {
       super();
@@ -14,25 +19,41 @@ public class DTextModel extends DShapeModel
       this.font = null;
    }
 
+   /**
+    * Getter method for the text of DTextModel.
+    * @return this.text - the text of the DTextModel.
+     */
    public String getText()
    {
       return this.text;
    }
 
+   /**
+    * Setter method for DTextModel.
+    * @param text - the text to set.
+     */
    public void setText(String text)
    {
       this.text = text;
-      notifyListeners();
+      notifyListeners(); //Notifies there has been a change.
    }
 
+   /**
+    * Getter method for the font of DTextModel.
+    * @return thisfont - the font of the DTextModel.
+     */
    public String getFont()
    {
       return this.font;
    }
 
+   /**
+    * Setter method for the font of the DTextModel.
+    * @param font - the text to set.
+    */
    public void setFont(String font)
    {
       this.font = font;
-      notifyListeners();
+      notifyListeners(); //Notifies there has been a change.
    }
 }
